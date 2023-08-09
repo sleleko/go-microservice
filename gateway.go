c5524cb22ac4d6a9dc154439dedbc270f31a6c4c
Sergey Leleko <sergey@leleko.ru>
2023-08-10 00:30:25 +0300

    Fixed build issues

--- gateway.go
+++ gateway.go
@@ -15,12 +15,29 @@ type Service struct {
 }
 
 func main() {
+	// for use via local machine
+	//services := []Service{
+	//	{Name: "service1", URL: "http://localhost:8081"},
+	//	{Name: "service2", URL: "http://localhost:8082"},
+	//}
+
+	// for use via Docker
 	services := []Service{
-		{Name: "service1", URL: "http://localhost:8081"},
-		{Name: "service2", URL: "http://localhost:8082"},
+		{Name: "service1", URL: "http://service1:8081"},
+		{Name: "service2", URL: "http://service2:8082"},
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			w.Header().Set("Content-Type", "text/html")
+			// Отображаем страницу приветствия
+			w.Write([]byte("This go microservice gateway, please select one of service to open:<br>"))
+			for _, service := range services {
+				w.Write([]byte(fmt.Sprintf("<a href='/%s' target='_blank'>%s</a><br>", service.Name, service.Name)))
+			}
+			return
+		}
+
 		for _, service := range services {
 			if r.URL.Path == "/"+service.Name {
 				serveReverseProxy(service.URL, w, r)
@@ -34,14 +51,14 @@ func main() {
 }
 
 func serveReverseProxy(target string, w http.ResponseWriter, r *http.Request) {
-	url, _ := url.Parse(target)
+	parsedURL, _ := url.Parse(target) // Изменил имя переменной на parsedURL
 
-	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy := httputil.NewSingleHostReverseProxy(parsedURL) // Используем parsedURL
 
-	r.URL.Host = url.Host
-	r.URL.Scheme = url.Scheme
+	r.URL.Host = parsedURL.Host
+	r.URL.Scheme = parsedURL.Scheme
 	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
-	r.Host = url.Host
+	r.Host = parsedURL.Host
 
 	proxy.ServeHTTP(w, r)
 }
